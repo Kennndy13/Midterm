657de34be81f35a50feda4ebcde3d8d529762e29
Kennndy13 <kvmanaig.wup@gmail.com>
2024-03-04 09:26:59 +0800

    Midterm

--- register.js
+++ register.js
@@ -1,6 +1,4 @@
-console.log("Hello World.", 43, 409.23, true, false, ["banana, 43, true"]);
-
-console.log(typeof("Hello World."), typeof(32234), typeof(["banana"]))
+console.log("Hello World.");
 
 // string = "asd", "kkkkkk", "2344"
 // number = 12345, 23.4, 60.5
